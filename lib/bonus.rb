486f84e6612805c45b22a321f545f39edbec994c
J K <kj688895@gmail.com>
2020-09-19 00:23:31 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,7 +20,11 @@ def bonus
    }
   }
 
-epic_tragedy[:montague][:montague][:hero][:status] = "dead"
+epic_tragedy[:montague][:hero][:status] = "dead"
+
+puts epic_tragedy
+
+epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 puts epic_tragedy
 
